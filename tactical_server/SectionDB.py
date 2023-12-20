41ead4c6bbe36d262fe843ecbd8bdf38d4d4212b
the-Tiege <thomasjohndunne97@gmail.com>
2023-12-20 11:11:47 +0000

    Model Tables DocStrings Updated

--- tactical_server/SectionDB.py
+++ tactical_server/SectionDB.py
@@ -29,10 +29,30 @@ Migrate(app,db)#create migration object.
 
 class Section(db.Model):
     """
-    Table for the overview of the section, tracks the overall state of the section.
+    Represents a section within the system, tracking various attributes and states.
+
+    This class corresponds to the 'Section' table in the database.
+
+    Attributes:
+        id (int): Identifying number of the section, serving as the primary key.
+        section_amunition (int): Total ammunition available in the section.
+        section_strength (int): Number of personnel in the section.
+        section_ok (int): Number of uninjured soldiers in the section.
+        section_casualty (int): Number of soldiers in the section who have become casualties.
+        section_location (str): Location of the section, obtained from the section commander.
+        section_battery (str): Battery level indication for the section database.
+        Soldier (relationship): One-to-many relationship to the 'Soldier' table.
+
+    Methods:
+        __init__(self, id, section_ammo):
+            Initializes a new Section instance.
+            
+            Args:
+                id (int): Identifying number of the section, must be unique.
+                section_ammo (int): Starting ammunition for the section.
     """
 
-    __tablename__ = 'Section'#name of the table.
+    __tablename__ = 'sections '#name of the table.
 
     id = db.Column(db.Integer,primary_key = True)#section number used as primary key.
     section_amunition = db.Column(db.Integer)#total ammunition in the section.
@@ -45,9 +65,13 @@ class Section(db.Model):
 
     def __init__(self,id,section_ammo):
         """
-        Function used to create a new section. To create the section, it requires an entry of an id number and ammunition.
-        :param id: Identifying number of the section, must be unique.
-        :param SectionAmmo: Sections starting ammunition.
+        Initializes a new section.
+
+        To create the section, it requires an entry of an id number and ammunition.
+
+        Args:
+            id (int): Identifying number of the section, must be unique.
+            section_ammo (int): Sections starting ammunition.
         """
         self.id = id#Identifying number of the section must be unique.
         self.section_amunition = section_ammo#Sections starting ammunition.
@@ -55,7 +79,37 @@ class Section(db.Model):
 
 class Soldier(db.Model):
     """
-    Table for soldiers in the section, gives more detailed information on each soldier.
+    Represents an individual soldier within the system, providing detailed information about each soldier.
+
+    This class corresponds to the 'soldiers' table in the database.
+
+    Attributes:
+        id (int): Army number of the soldier, serving as the primary key. Must be unique.
+        name (str): Soldier's name.
+        role (str): Role of the section member.
+        section_id (int): ID number of the section that the soldier belongs to.
+        identity_check (str): Used to verify the soldier's identity.
+        vitals (relationship): One-to-many relationship to the 'vitals' table, listing the soldier's heart rate over time.
+        location_history (relationship): One-to-many relationship to the 'location' table, listing the soldier's location over time.
+        current_location (str): Stores the current location of the soldier.
+        last_heart_rate (int): Stores the most recent record of the soldier's heart rate.
+        last_update_time (str): Stores the time of the last update.
+        ammunition_expended (int): Stores the number of rounds fired by the soldier.
+        rifle_sensor_battery_level (str): Stores the battery level of the ammunition tracker.
+        status (str): Stores the status of the soldier.
+        armour_sensor_battery_level (str): Stores the battery level of the body armour sensor.
+        hub_sensor_battery_level (str): Stores the battery level of the sensor hub.
+        distance_traveled (str): Stores the distance traveled by the soldier.
+
+    Methods:
+        __init__(self, id, name, role, section_id):
+            Initializes a new Soldier instance.
+            
+            Args:
+                id (int): Army number, must be unique.
+                name (str): Soldier's name.
+                role (str): Soldier's role in the section.
+                section_id (int): Section ID to which the soldier belongs.
     """
 
     __tablename__='soldiers'#Table name.
@@ -80,12 +134,16 @@ class Soldier(db.Model):
 
     def __init__(self,id,name,role,section_id):
         """
-        Function used to create a new section member. To add a soldier requires an entry of an id number, a name,
-        the role in the section, and the section they are in.
-        :param id: Army number.
-        :param name: Soldier's name.
-        :param role: Soldier's role in the section.
-        :param section_id: Section the soldier belongs to.
+        Initializes a new section member.
+
+        To add a soldier, it requires an entry of an army number, a name,
+        the role in the section, and the section they belong to.
+
+        Args:
+            id (int): Army number, must be unique.
+            name (str): Soldier's name.
+            role (str): Soldier's role in the section.
+            section_id (int): Section the soldier belongs to.
         """
         self.id = id#Army number.
         self.name=name#Soldiers name.
@@ -95,16 +153,20 @@ class Soldier(db.Model):
 
 class Vitals(db.Model):
     """
-    Table for a record of a soldier's vital signs.
+    Represents a table for recording a soldier's vital signs, particularly heart rate.
+
+    This class corresponds to the 'vitals' table in the database.
 
     Attributes:
-    - id (int): ID number of the entry in the database, automatically generated unique primary key.
-    - HR (int): Heart rate of the soldier.
-    - TIME (str): Time when the heart rate entry was made.
-    - soldier_id (int): ID number of the soldier to whom the entry belongs.
+        id (int): Automatically generated unique primary key for the database entry.
+        heart_rate (int): Heart rate of the soldier.
+        update_time (str): Time when the heart rate entry was made.
+        soldier_id (int): ID number of the soldier to whom the entry belongs.
 
     Methods:
-    - __init__(self, HR, TIME, soldier_id): Initializes a new entry for heart rate for a soldier.
+        __init__(self, heart_rate, update_time, soldier_id):
+            Initializes a new entry for heart rate for a soldier.
+
     """
 
     __tablename__='vitals'#Name of table.
@@ -117,12 +179,12 @@ class Vitals(db.Model):
     #function used to create a new entry for heart rate for soldier.Automatically created from data sent from sensors.
     def __init__(self, heart_rate, time, soldier_id):
         """
-        Initializes a new entry for heart rate for a soldier. Automatically created from data sent from sensors.
+        Initializes a new entry for heart rate for a soldier.
 
         Parameters:
-        - HR (int): Heart rate.
-        - TIME (str): Time of the entry.
-        - soldier_id (int): ID number of the soldier that the entry belongs to.
+            heart_rate (int): Heart rate.
+            update_time (str): Time of the entry.
+            soldier_id (int): ID number of the soldier that the entry belongs to.
         """
 
         self.heart_rate = heart_rate#Heart rate.
@@ -133,20 +195,22 @@ class Vitals(db.Model):
 
 class Location(db.Model):
     """
-    SQLAlchemy Model: Location
+    Represents a table for recording the locations a soldier has been.
 
-    Table for a record of all locations a soldier has been.
+    This class corresponds to the 'locations' table in the database.
 
     Attributes:
-    - id (int): ID number of the entry in the database, automatically generated unique primary key.
-    - long (str): Longitude.
-    - lat (str): Latitude.
-    - GRID (str): Grid reference.
-    - TIME (str): Time when the location entry was made.
-    - soldier_id (int): ID number of the soldier.
+        id (int): Automatically generated unique primary key for the database entry.
+        long (str): Longitude of the soldier's location.
+        lat (str): Latitude of the soldier's location.
+        grid_reference (str): Grid reference of the soldier's location.
+        update_time (str): Time when the location entry was made.
+        soldier_id (int): ID number of the soldier to whom the entry belongs.
 
     Methods:
-    - __init__(self, long, lat, GRID, TIME, soldier_id): Initializes a new entry for the location of a soldier.
+        __init__(self, long, lat, grid_reference, update_time, soldier_id):
+            Initializes a new entry for the location of a soldier.
+
     """
 
     __tablename__ = 'locations'#Table name.
@@ -160,6 +224,16 @@ class Location(db.Model):
 
     #function used to create a new entry for location of soldier.Automatically created from data sent from sensors.
     def __init__(self,long,lat,GRID,TIME,soldier_id):
+         """
+        Initializes a new entry for the location of a soldier.
+
+        Parameters:
+            long (str): Longitude of the soldier's location.
+            lat (str): Latitude of the soldier's location.
+            grid_reference (str): Grid reference of the soldier's location.
+            update_time (str): Time when the location entry was made.
+            soldier_id (int): ID number of the soldier to whom the entry belongs.
+        """
          self.long = long#Longitude.
          self.lat =lat#Latitude.
          self.grid_reference = GRID#Grid reference.
