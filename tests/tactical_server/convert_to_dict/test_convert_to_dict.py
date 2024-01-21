af3ed4b8d761d185950a4965f374e61d317797c7
the-Tiege <thomasjohndunne97@gmail.com>
2024-01-21 15:07:49 +0000

    unit test corrected after initial merge

--- tests/tactical_server/convert_to_dict/test_convert_to_dict.py
+++ tests/tactical_server/convert_to_dict/test_convert_to_dict.py
@@ -5,25 +5,18 @@ from unittest.mock import patch, MagicMock
 TEST_DATA = [
     (
         "ID:862097,lat:52.65913,long:-8.62513",
-        ({'ID': '862097', 'lat': '52.65913', 'long': '-8.62513' , 'number':1}, "Some Value")
+        ({'ID': '862097', 'lat': '52.65913', 'long': '-8.62513'})
     ),
     (
         "id:862097,lat:52.65913,long:-8.62513",
-        ({'id': 862097, 'lat': '52.65913', 'long': '-8.62513', 'number':1}, "Some Value")
+        ({'id': 862097, 'lat': '52.65913', 'long': '-8.62513'})
     )
 ]
-@pytest.fixture
-def setup():
-    x = 1
-    return x
 
 
 @pytest.mark.parametrize("message, expected_result", TEST_DATA)
-@patch("tactical_server.convert_to_dict.convert_to_dict.number_yoke")
-def test_convert_to_dict(mock_number_yoke, message, expected_result, setup):
-    mock_number_yoke.return_value = 1
-    test_mock = MagicMock(return_value="Some Value")
-    result = convert_to_dict(message, test_mock)
+def test_convert_to_dict( message, expected_result):
+    result = convert_to_dict(message)
 
     assert result == expected_result
 
